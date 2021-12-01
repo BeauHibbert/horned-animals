21977843e49afd8824ce5ef9ec1f38b6813a350c
Beau Hibbert <beauhibbert789@gmail.com>
2021-11-30 22:19:18 -0800

    add state to horned beast and render heart image and times clicked for each beast

--- README.md
+++ README.md
@@ -2,11 +2,11 @@ Number and name of feature: feature #1 Display images
 
 Estimate of time needed to complete: 30 min
 
-Start time: _____
+Start time: 9
 
-Finish time: _____
+Finish time: 9:40
 
-Actual time needed to complete: _____
+Actual time needed to complete: 40 min
 
 Number and name of feature: Feature #2 Allow users to vote for their favorite beast
 
--- src/HornedBeast.js
+++ src/HornedBeast.js
@@ -1,12 +1,29 @@
 import {Component} from 'react';
+import heart from './images/heart.png';
 
 class HornedBeast extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      timesClicked: 0,
+    };
+  }
+
+  handleClick = () => {
+    this.setState({timesClicked: this.state.timesClicked + 1});
+  }
+
   render() {
-    console.log('imageUrl in HB: ', this.props.imageUrl);
     return(
       <>
         <h2>{this.props.title}</h2>
-        <img src={this.props.imageUrl} alt={this.props.description} title={this.props.title}/>
+        <img src={this.props.imageUrl} alt={this.props.description} title={this.props.title} onClick={this.handleClick}/>
+        <div id="heart-image-wrapper">
+          <img src={heart} alt="Red heart image" id="heart-image"/>
+          <p>{this.state.timesClicked}</p>
+        </div>
+        
         <p>{this.props.description}</p>
       </>
     );
