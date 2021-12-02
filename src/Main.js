7893c19cda3078252ef5f15c9bef5237c3304626
Beau Hibbert <beauhibbert789@gmail.com>
2021-12-01 16:42:49 -0800

    implement pr suggestions

--- src/Main.js
+++ src/Main.js
@@ -5,13 +5,11 @@ import {Container, Row, Col} from 'react-bootstrap';
 
 class Main extends Component {
   render() {
-    let newBeasts = [];
-    data.forEach(function(beast) {
-      let newBeast = <HornedBeast title={beast.title} imageUrl={beast.image_url} description={beast.description}></HornedBeast>;
-      let newBeastCol = <Col xs={12} md={6} lg={4}>{newBeast}</Col>;
-      newBeasts.push(newBeastCol);
-    }
-    );
+    let newBeasts = data.map(beast => {
+      console.log(beast);
+      let newBeast = <HornedBeast title={beast.title} imageUrl={beast.image_url} description={beast.description} key={beast.title}></HornedBeast>;
+      return <Col xs={12} md={6} lg={4}>{newBeast}</Col>;
+    });
 
     return(
       <Container>
