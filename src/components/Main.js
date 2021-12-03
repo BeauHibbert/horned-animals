10dd88454c7eae05bd53a5da2063c74797804b4f
Beau Hibbert <beauhibbert789@gmail.com>
2021-12-02 20:27:34 -0800

    the entire lab

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,29 +1,53 @@
 import {Component} from 'react';
 import HornedBeast from './HornedBeast.js';
-import {Container, Row, Col} from 'react-bootstrap';
-// import SelectedBeast from './SelectedBeast.js';
+import {Container, Row, Col, Form} from 'react-bootstrap';
 
 class Main extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      // showModal: false
+      beastsToShow: this.props.imageData,
     };
   }
 
+  handleChange = (event) => {
+    const selection = parseInt(event.target.value);
+    let filteredBeasts;
+
+    if (selection === 1) {
+      filteredBeasts = this.props.imageData.filter(beast => beast.horns === 1);
+    } else if (selection === 2) {
+      filteredBeasts = this.props.imageData.filter(beast => beast.horns === 2);
+    } else if (selection === 3) {
+      filteredBeasts = this.props.imageData.filter(beast => beast.horns === 3);
+    } else {
+      filteredBeasts = this.props.imageData.filter(beast => beast.horns === 100);
+    }
+
+    this.setState({beastsToShow: filteredBeasts});
+  }
 
   render() {
-    let newBeasts = this.props.imageData.map(beast => {
-      console.log(beast);
+    let beasts = this.state.beastsToShow.map(beast => {
       let newBeast = <HornedBeast title={beast.title} imageUrl={beast.image_url} description={beast.description} key={beast.title}></HornedBeast>;
       return <Col xs={12} md={6} lg={4}>{newBeast}</Col>;
     });
 
     return(
       <>
+        <Form>
+          <Form.Label>Select the Number of Horns You Would Like to See</Form.Label>
+          <Form.Select aria-label='Horn Selector' onChange={this.handleChange}>
+            <option>Select a number of horns</option>
+            <option value="1">One-horned beasts</option>
+            <option value="2">Two-horned beasts</option>
+            <option value="3">Three-horned beasts</option>
+            <option value="100">One-hundred-horned beasts</option>
+          </Form.Select>
+        </Form>
         <Container>
           <Row>
-            {newBeasts}
+            {beasts}
           </Row>
         </Container>
       </>
