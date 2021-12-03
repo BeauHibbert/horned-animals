f4999a378d85a7b2e4f76226c13f99d20e2b73f7
Beau Hibbert <beauhibbert789@gmail.com>
2021-12-02 21:18:30 -0800

    fixed netflify compile errors

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {Component} from "react";
+import React, {Component} from 'react';
 import './App.css';
 import Header from './components/Header.js';
 import Main from './components/Main.js';
@@ -14,7 +14,7 @@ export default class App extends Component {
         <Footer></Footer>
       </>
     );
-  } 
+  }
 }
 
 
